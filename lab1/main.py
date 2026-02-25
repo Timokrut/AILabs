fc3fd118bda49cf5560fe8b05450ef5c566ed9db
Timokrut <potapov.tim14@gmail.com>
2026-02-25 17:47:44 +0300

    2. n = 4

--- lab1/main.py
+++ lab1/main.py
@@ -3,7 +3,7 @@ from numpy import exp, array, random, dot
 class NeuralNetwork():
     def __init__(self):
         random.seed(1)
-        self.synaptic_weights = 2 * random.random((3, 1)) - 1
+        self.synaptic_weights = 2 * random.random((4, 1)) - 1
 
     def __sigmoid(self, x):
         return 1 / (1 + exp(-x))
@@ -27,13 +27,19 @@ if __name__ == "__main__":
     print("Random starting synaptic weights: ")
     print(neural_network.synaptic_weights)
 
-    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+    training_set_inputs = array([
+        [0, 0, 1, 0], 
+        [1, 1, 1, 0], 
+        [1, 0, 1, 1], 
+        [0, 1, 1, 1]
+    ])
+
     training_set_outputs = array([[0, 1, 1, 0]]).T
 
     neural_network.train(training_set_inputs, training_set_outputs, 10000)
     print("New synaptic weights after training: ")
     print(neural_network.synaptic_weights)
 
-    print("Considering new situation [1, 0, 0] -> ?: ")
-    print(neural_network.think(array([1, 0, 0])))
+    print("Considering new situation [1, 0, 0, 1] -> ?: ")
+    print(neural_network.think(array([1, 0, 0, 1])))
 
